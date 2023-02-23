f758e0aa6265709a0669467a29c7372e9297e8e4
Narko <nekit.2280.3390@gmail.com>
2023-02-24 02:22:10 +0300

    Elements initialized by program code

--- app/src/main/java/com/example/android_pract_2/MainActivity.java
+++ app/src/main/java/com/example/android_pract_2/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.android_pract_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageView = findViewById(R.id.dr_livsy);
+        imageView.setImageResource(R.drawable.beer);
+
+        EditText emailField = findViewById(R.id.editText_email);
+        emailField.setText(R.string.password);
+
+        EditText passwordField = findViewById(R.id.editText_password);
+        passwordField.setText(R.string.password);
     }
 }
